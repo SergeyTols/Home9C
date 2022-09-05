09a3ad007ba1f02ae3049206b0d31b17f0d594f0
Sarge <sarge2033@gmail.com>
2022-09-05 13:55:03 +0300

    Задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿int m = InputNumbers("Введите число (m): ");
+int n = InputNumbers("Введите число (n): ");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+if (m <= n && n > 0)
+{   
+    Console.Write("Натуральные числа от m до n: \"\"");
+
+        for(int i = m; i < n; i++)
+        {
+            if(i > 0) Console.Write(i + ", ");
+        }  
+         
+    Console.Write(n + "\"\"");
+}
+else Console.Write("Натуральных чисел нет между ними");
