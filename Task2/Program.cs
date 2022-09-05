49d1f752d19548a5183057a69db6c7902784da1a
Sarge <sarge2033@gmail.com>
2022-09-05 13:55:15 +0300

    Задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿int m = InputNumbers("Введите число (m): ");
+int n = InputNumbers("Введите число (n): ");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int sum = 0;
+if (m <= n && n > 0)
+{    
+    Console.Write("Натуральные числа от m до n: \"\"");
+    
+        for(int i = m; i < n; i++)
+        {
+            if(i > 0)
+            {
+                Console.Write(i + ", ");
+                sum += i;
+            } 
+        } 
+    sum += n;    
+    Console.Write(n + "\"\"");
+    Console.Write($"\nСумма натуральных чисел между {m} и {n} : {sum}");
+}
+else Console.Write("Натуральных чисел нет между ними");
+
