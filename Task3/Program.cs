181068b21b405ccd8942911926816e6690659f67
Sarge <sarge2033@gmail.com>
2022-09-08 17:52:02 +0300

    Задание 3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int m = InputNumbers("Введите положительное число m: ");
+int n = InputNumbers("Введите положительное число n: ");
+
+int FunctionAkkerman = Akk(m, n);
+
+Console.Write("Функция Аккермана = " + FunctionAkkerman);
+
+int Akk(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return Akk(m - 1, 1);
+  else return Akk(m - 1, Akk(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
